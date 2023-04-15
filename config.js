c14b1f10996bbc095d334bb9c568ec5f8edb3a8a
David Antonio Arenas Islas <davidantarenas@icloud.com>
2023-04-15 13:39:39 -0600

    db Settings 'Connection.js'

--- config.js
+++ config.js
@@ -2,6 +2,8 @@ import {config} from "dotenv";
 config();
 
 export default {
+
+    
     port: process.env.PORT || 3000,
     
 }
--- src/database/connection.js
+++ src/database/connection.js
@@ -1,10 +1,19 @@
-import sql from 'mssql'
+import sql from "mssql";
 
-const dbSettings = { 
-    user 
-    password
-    server
-    database
- }
+const dbSettings = {
+  user: "sa",
+  password: "Aeaa5703$",
+  server: "localhoast",
+  database: "LaColmena",
+  options: {
+    encrypt: true, // for azure
+    trustServerCertificate: true, // change to true for local dev / self-signed certs
+  },
+};
+async function getConnection() {
+  const pool = await sql.connect(dbSettings);
+  const result = await pool.request().query("SELECT 1");
+  console.log(result);
+}
 
- sql.connect()
+getConnection();
